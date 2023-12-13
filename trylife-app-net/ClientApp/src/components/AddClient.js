1212afb9c7945bf3ccb216d292fdd0c792629bff
mdonatelli22 <DonatelliME20@gcc.edu>
2023-12-12 23:01:12 -0500

    importing clients

--- trylife-app-net/ClientApp/src/components/AddClient.js
+++ trylife-app-net/ClientApp/src/components/AddClient.js
@@ -1,16 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export class AddClient extends Component {
-  static displayName = AddClient.name;
 
-  render() {
-    return (
-      <div>
-       <h1>Hello, world!</h1>
-      </div>
-    );
-  }
+
+export const AddClient = () => {
+  const [client, setClient] = useState([]);
+  const dataType = 1;
+
+  useEffect(() => {
+    fetch(`item/${dataType}`)
+    .then((results) => {
+      return results.json();
+    })
+    .then(data => {
+      setClient(data);
+    })
+  }, [])
+
+  return(
+    <main>
+      {
+        (client != null)?client.map((client) => <h3>{client.name}</h3>):<div>Loading...</div>
+      }
+
+    </main>
+  )
 }
 
+ 
 
+export default AddClient;
 
