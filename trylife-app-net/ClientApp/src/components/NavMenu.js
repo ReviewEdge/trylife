227888d7c0b6dcc5437bbb0abb0afcf1044cacb6
Logan <reviewedge56@gmail.com>
2023-12-13 23:44:51 -0500

    view products from local json

--- trylife-app-net/ClientApp/src/components/NavMenu.js
+++ trylife-app-net/ClientApp/src/components/NavMenu.js
@@ -38,6 +38,9 @@ export class NavMenu extends Component {
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
               </NavItem>
+              <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/view-products">Products</NavLink>
+              </NavItem>
             </ul>
           </Collapse>
         </Navbar>
